a5faa842ec8606a0b799eb44495804d6757d801c
Nirmal Alex <nirmal@billionlives.in>
2022-05-26 22:41:14 +0530

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,11 @@
 import React from "react";
-var a = 10;
-var b = 100;
-var d=0;
-let c = {
-  backgroundColor: "green",
-  width: 300,
-  height: 300
+import StateVariables from "./StateVariables";
+export default class Akash extends React.Component {
 
-};
-
-
-
-export default class Akash extends React.Component{
-
-  showContent = () => {
-    a = 20;
-    b = 500;
-    console.log(a);
-    console.log(b);
-  }
-
-  render(){
-    b = 200;
-    return(
-      <div 
-        style={{
-          backgroundColor: a,
-          width: 300,
-          height: 300
-        }}
-      >
-        Hello world
-        <button onClick={this.showContent}>
-          click me 
-        </button>
-        {a}
-        {b}
+  render() {
+    return (
+      <div>
+        <StateVariables />
       </div>
     )
   }
